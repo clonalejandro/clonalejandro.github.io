30bf2d959950b72ba8a265bc854614cd9ebcd0e6
clonalejandro <contact@clonalejandro.me>
2019-07-30 04:00:15 +0200

    Publish my web page test

---
+++ precache-manifest.b98e81d2b96e7687c41c4643897f3830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cff987562feb2b7d14182d8ac0ba3ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d62e17cb0993d95718c",
+    "url": "/static/css/2.6a6cc977.chunk.css"
+  },
+  {
+    "revision": "2eaca04179271264f040",
+    "url": "/static/css/main.7a712006.chunk.css"
+  },
+  {
+    "revision": "5d62e17cb0993d95718c",
+    "url": "/static/js/2.47453993.chunk.js"
+  },
+  {
+    "revision": "2eaca04179271264f040",
+    "url": "/static/js/main.b8b6e034.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
