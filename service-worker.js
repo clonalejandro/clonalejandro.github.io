21a367e985ea48c308a9e5acd25fa880e3b98277
clonalejandro <clonalejandromcb@gmail.com>
2022-05-09 11:39:06 +0200

    Fix .env vars

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d40c39b6054752b136f47ffcf3ea6a06.js"
+  "/precache-manifest.61798fd3edd946face26dd10a6f000ff.js"
 );
 
 self.addEventListener('message', (event) => {
